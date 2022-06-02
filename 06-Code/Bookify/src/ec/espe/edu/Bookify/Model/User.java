3a755b9ca3de48452da388821e1616edbbb5eb88
Jdquimbiulco <juandiegoq21@gmail.com>
2022-06-01 22:06:44 -0500

    user

--- 06-Code/Bookify/src/ec/espe/edu/Bookify/Model/User.java
+++ 06-Code/Bookify/src/ec/espe/edu/Bookify/Model/User.java
@@ -6,8 +6,87 @@ package ec.espe.edu.Bookify.Model;
 
 /**
  *
- * @author QUILUMBAQUIN JAIRO, DCC0-ESPE: CODEX++
+ * @author QUILUMBAQUIN JAIRO,QUIMBIULCO JUAN,RAUL SILVA,CARLOS RIVERA, DCC0-ESPE: CODEX++
  */
 public class User {
+    private String name;
+    private int id;
+    private String userEmail;
+    private int userPhone;
+    private int userAddress; 
+    private int age;
+    private int password;
+    
+   
+    public User(String name, int id, String userEmail, int userPhone, int userAddress, int age, int password) {
+        this.name = name;
+        this.id = id;
+        this.userEmail = userEmail;
+        this.userPhone = userPhone;
+        this.userAddress = userAddress;
+        this.age = age;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public int getUserPhone() {
+        return userPhone;
+    }
+
+    public void setUserPhone(int userPhone) {
+        this.userPhone = userPhone;
+    }
+
+    public int getUserAddress() {
+        return userAddress;
+    }
+
+    public void setUserAddress(int userAddress) {
+        this.userAddress = userAddress;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getPassword() {
+        return password;
+    }
+
+    public void setPassword(int password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "name=" + name + ", id=" + id + ", userEmail=" + userEmail + ", userPhone=" + userPhone + ", userAddress=" + userAddress + ", age=" + age + ", password=" + password + '}';
+    }
     
 }
