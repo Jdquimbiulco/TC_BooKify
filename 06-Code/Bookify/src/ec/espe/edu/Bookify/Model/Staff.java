bb835e9a751690338f1bc0b09dbac8d8846ec9bc
Jairo Quilumbaquin <jsquilumbaquin@espe.edu.ec>
2022-06-13 22:48:25 -0500

    HelpMeGod

---
+++ 06-Code/Bookify/src/ec/espe/edu/Bookify/Model/Staff.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.espe.edu.Bookify.Model;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+/**
+ *
+ * @author QUILUMBAQUIN JAIRO, DCC0-ESPE: CODEX++
+ */
+public class Staff {
+    
+}
