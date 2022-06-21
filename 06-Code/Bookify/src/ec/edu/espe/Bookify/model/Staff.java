e48fba08b4b91758408df955840f0a93bff8677a
Jairo Quilumbaquin <jsquilumbaquin@espe.edu.ec>
2022-06-21 13:17:27 -0500

    V1.1

--- 06-Code/Bookify/src/ec/edu/espe/Bookify/model/Staff.java
+++ 06-Code/Bookify/src/ec/edu/espe/Bookify/model/Staff.java
@@ -4,11 +4,9 @@
  */
 package ec.edu.espe.Bookify.model;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
